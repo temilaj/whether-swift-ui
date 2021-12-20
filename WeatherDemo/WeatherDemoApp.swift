f1f5bab7dcbe2a7395e923d3a3cf4f06ab150dc9
temilaj <temilaj@hotmail.com>
2021-12-20 13:27:18 -0500

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Temi Lajumoke on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
